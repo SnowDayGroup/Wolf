a338bb085fc63c9482add65f3429adc942a2e823
Brad Parbs <brad@bradparbs.com>
2013-06-09 15:19:37 -0500

    upstream changes from _s -> image.php fixes

--- image.php
+++ image.php
@@ -15,7 +15,7 @@ get_header();
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
-					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 					<div class="entry-meta">
 						<?php
@@ -30,8 +30,7 @@ get_header();
 								esc_attr( strip_tags( get_the_title( $post->post_parent ) ) ),
 								get_the_title( $post->post_parent )
 							);
-						?>
-						<?php edit_post_link( __( 'Edit', 'wolf_starter' ), '<span class="sep"> | </span> <span class="edit-link">', '</span>' ); ?>
+						edit_post_link( __( 'Edit', '_s' ), '<span class="sep"> | </span> <span class="edit-link">', '</span>' ); ?>
 					</div><!-- .entry-meta -->
 
 					<nav role="navigation" id="image-navigation" class="navigation-image">
@@ -41,40 +40,9 @@ get_header();
 				</header><!-- .entry-header -->
 
 				<div class="entry-content">
-
 					<div class="entry-attachment">
 						<div class="attachment">
-							<?php
-								/**
-								 * Grab the IDs of all the image attachments in a gallery so we can get the URL of the next adjacent image in a gallery,
-								 * or the first image (if we're looking at the last image in a gallery), or, in a gallery of one, just the link to that image file
-								 */
-								$attachments = array_values( get_children( array(
-									'post_parent'    => $post->post_parent,
-									'post_status'    => 'inherit',
-									'post_type'      => 'attachment',
-									'post_mime_type' => 'image',
-									'order'          => 'ASC',
-									'orderby'        => 'menu_order ID'
-								) ) );
-								foreach ( $attachments as $k => $attachment ) {
-									if ( $attachment->ID == $post->ID )
-										break;
-								}
-								$k++;
-								// If there is more than 1 attachment in a gallery
-								if ( count( $attachments ) > 1 ) {
-									if ( isset( $attachments[ $k ] ) )
-										// get the URL of the next image attachment
-										$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
-									else
-										// or get the URL of the first image attachment
-										$next_attachment_url = get_attachment_link( $attachments[ 0 ]->ID );
-								} else {
-									// or, if there's only 1 image, get the URL of the image
-									$next_attachment_url = wp_get_attachment_url();
-								}
-							?>
+							<?php _wolf_starter_the_attached_image(); ?>
 
 							<a href="<?php echo esc_url($next_attachment_url); ?>" title="<?php the_title_attribute(); ?>" rel="attachment"><?php
 								$attachment_size = apply_filters( 'wolf_starter_attachment_size', array( 1200, 1200 ) ); // Filterable image size.
@@ -89,8 +57,8 @@ get_header();
 						<?php endif; ?>
 					</div><!-- .entry-attachment -->
 
-					<?php the_content(); ?>
-					<?php 
+					<?php
+						the_content();
 						wp_link_pages( array( 
 							'before' => '<div class="page-links">' . __( 'Pages:', 'wolf_starter' ), 
 							'after' => '</div>',
@@ -100,16 +68,19 @@ get_header();
 				</div><!-- .entry-content -->
 
 				<footer class="entry-meta">
-					<?php if ( comments_open() && pings_open() ) : // Comments and trackbacks open ?>
-						<?php printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'wolf_starter' ), get_trackback_url() ); ?>
-					<?php elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open ?>
-						<?php printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'wolf_starter' ), get_trackback_url() ); ?>
-					<?php elseif ( comments_open() && ! pings_open() ) : // Only comments open ?>
-						<?php _e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', 'wolf_starter' ); ?>
-					<?php elseif ( ! comments_open() && ! pings_open() ) : // Comments and trackbacks closed ?>
-						<?php _e( 'Both comments and trackbacks are currently closed.', 'wolf_starter' ); ?>
-					<?php endif; ?>
-					<?php edit_post_link( __( 'Edit', 'wolf_starter' ), ' <span class="edit-link">', '</span>' ); ?>
+				<?php
+					if ( comments_open() && pings_open() ) : // Comments and trackbacks open
+						printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', '_s' ), get_trackback_url() );
+					elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open
+						printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', '_s' ), get_trackback_url() );
+					elseif ( comments_open() && ! pings_open() ) : // Only comments open
+						_e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', '_s' );
+					elseif ( ! comments_open() && ! pings_open() ) : // Comments and trackbacks closed
+						_e( 'Both comments and trackbacks are currently closed.', '_s' );
+					endif;
+					
+					edit_post_link( __( 'Edit', '_s' ), ' <span class="edit-link">', '</span>' );
+				?>
 				</footer><!-- .entry-meta -->
 			</article><!-- #post-<?php the_ID(); ?> -->
 
