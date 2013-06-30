8d09b574f6708d956589b3e78ce0203a724469cb
Brad Parbs <brad@bradparbs.com>
2013-06-30 14:20:48 -0500

    removed duplicate search label

--- searchform.php
+++ searchform.php
@@ -7,7 +7,6 @@
 ?>
 <?php do_action('wolf_before_search_form'); ?>
 	<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-		<label for="s" class="screen-reader"><?php _ex( 'Search', 'assistive text', 'wolf' ); ?></label>
 		<label for="s" class="screen-reader-text"><?php _ex( 'Search', 'assistive text', 'wolf' ); ?></label>
 		<input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'wolf' ); ?>" />
 		<input type="submit" class="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'wolf' ); ?>" />
