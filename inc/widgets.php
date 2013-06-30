e97af32d5b48b62b4d688ebac2faebda91052c0c
Brad Parbs <brad@bradparbs.com>
2013-06-30 14:20:28 -0500

    Added widgets to the footer, including styling

--- inc/widgets.php
+++ inc/widgets.php
@@ -5,7 +5,31 @@
 function wolf_widgets_init() {
     register_sidebar( array(
         'name'          => __( 'Sidebar', 'wolf' ),
-        'id'            => 'sidebar-1',
+        'id'            => 'sidebar-primary',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ),
+    array(
+        'name'          => __( 'Left Footer Sidebar', 'wolf' ),
+        'id'            => 'sidebar-left-footer',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ),
+    array(
+        'name'          => __( 'Center Footer Sidebar', 'wolf' ),
+        'id'            => 'sidebar-center-footer',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ),
+    array(
+        'name'          => __( 'Right Footer Sidebar', 'wolf' ),
+        'id'            => 'sidebar-right-footer',
         'before_widget' => '<aside id="%1$s" class="widget %2$s">',
         'after_widget'  => '</aside>',
         'before_title'  => '<h1 class="widget-title">',
