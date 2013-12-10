8d1893749d8379616d841bcbdb22a456cbfe40ab
Ryan Johnston <ryanjohnston@gmail.com>
2013-12-10 16:01:40 -0600

    Update widgets.php
    Theme not initializing due to missing closing parens and extra 'array(' strings.

--- inc/widgets.php
+++ inc/widgets.php
@@ -10,27 +10,24 @@ function wolf_widgets_init() {
         'after_widget'  => '</aside>',
         'before_title'  => '<h1 class="widget-title">',
         'after_title'   => '</h1>',
-    );
+    ) );
     register_sidebar( array(
-    array(
         'name'          => __( 'Left Footer Sidebar', 'wolf' ),
         'id'            => 'sidebar-left-footer',
         'before_widget' => '<aside id="%1$s" class="widget %2$s">',
         'after_widget'  => '</aside>',
         'before_title'  => '<h1 class="widget-title">',
         'after_title'   => '</h1>',
-    );
+    ) );
     register_sidebar( array(
-    array(
         'name'          => __( 'Center Footer Sidebar', 'wolf' ),
         'id'            => 'sidebar-center-footer',
         'before_widget' => '<aside id="%1$s" class="widget %2$s">',
         'after_widget'  => '</aside>',
         'before_title'  => '<h1 class="widget-title">',
         'after_title'   => '</h1>',
-     );
+    ) );
     register_sidebar( array(
-    array(
         'name'          => __( 'Right Footer Sidebar', 'wolf' ),
         'id'            => 'sidebar-right-footer',
         'before_widget' => '<aside id="%1$s" class="widget %2$s">',
